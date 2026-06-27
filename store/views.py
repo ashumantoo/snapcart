3a609ee7aa13d4eeddef7c96d06e1552d19a8b59
Ashutosh Kumar Mantoo <ashumantoo@gmail.com>
2026-06-27 11:05:26 +0530

    search product functionality

--- store/views.py
+++ store/views.py
@@ -1,4 +1,8 @@
+from ast import If
+
 from django.core.paginator import Paginator
+from django.db.models import Q
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from carts.models import CartItem
@@ -11,15 +15,23 @@ from store.models import Product
 def store(request, category_slug=None):
     if category_slug != None:
         category = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.all().filter(is_available=True, category=category).order_by('id')
+        products = (
+            Product.objects.all()
+            .filter(is_available=True, category=category)
+            .order_by("id")
+        )
         paginator = Paginator(products, 1)
-        page = request.GET.get("page")  # This we will get from frontend with the url like 'example.com/store/page=1'
+        page = request.GET.get(
+            "page"
+        )  # This we will get from frontend with the url like 'example.com/store/page=1'
         paged_products = paginator.get_page(page)
 
     else:
-        products = Product.objects.all().filter(is_available=True).order_by('id')
+        products = Product.objects.all().filter(is_available=True).order_by("id")
         paginator = Paginator(products, 3)
-        page = request.GET.get("page")  # This we will get from frontend with the url like 'example.com/store/page=1'
+        page = request.GET.get(
+            "page"
+        )  # This we will get from frontend with the url like 'example.com/store/page=1'
         paged_products = paginator.get_page(page)
 
     product_count = products.count()
@@ -40,3 +52,22 @@ def product_details(request, category_slug, product_slug):
 
     context = {"product": product, "in_cart": in_cart}
     return render(request, "product-details.html", context)
+
+
+def search(request):
+    if "keyword" in request.GET:
+        search_keyword = request.GET["keyword"]
+        if search_keyword:
+            # icontains = It will match the keyword with the description
+            # Q - to query the complex query over the data
+            # 👇🏽simple filter query with single parameter
+            # products = Product.objects.order_by('-created_date').filter(description__icontains=search_keyword)
+
+            # 👇🏽complex filter query over multiple parameter, need to use Q
+            products = Product.objects.order_by("-created_date").filter(
+                Q(description__icontains=search_keyword)
+                | Q(product_name__icontains=search_keyword)
+            )
+            product_count = products.count()
+    context = {"products": products, "product_count": product_count}
+    return render(request, "store.html", context)
